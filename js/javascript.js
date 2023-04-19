aaa1efe889318cb5a25bd2af8d963b442d253f1f
valmirfilho <valmir@valmirsergio.com>
2023-04-19 17:21:45 -0300

    função descriptograr adicionada

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,49 @@
+
+function criptografar(texto) {
+    let resultado = ""
+    
+    for ( let letra of texto ) {
+        
+
+        if(letra === "a") {
+            letra = "ai"
+
+        }
+        else if(letra === "e") {
+            letra = "enter"
+            
+        }
+        else if(letra === "i") {
+            letra = "imes"
+            
+        }
+        else if(letra === "o") {
+            letra = "ober"
+            
+        }
+        else if(letra === "u") {
+            letra = "ufat"
+            
+        }
+      
+        resultado += letra
+        
+    } 
+    console.log(resultado)
+    return resultado
+    
+}
+function descriptografar(textoParaDescriptografar) {
+    let textoDescriptografado = textoParaDescriptografar.replace(/enter/g, "e")
+                                                        .replace(/imes/g, "i")
+                                                        .replace(/ai/g, "a")
+                                                        .replace(/ober/g, "o")
+                                                        .replace(/ufat/g, "u")
+                                                     
+    return textoDescriptografado;
+}
+
+
 const textoCriptografar = document.getElementById("texto-criptografar")
 const enviarCriptografia = document.getElementById("enviar-Criptografia")
 
@@ -5,39 +51,19 @@ const enviarCriptografia = document.getElementById("enviar-Criptografia")
 
 enviarCriptografia.addEventListener('click', function() {
 
-    textoParaCriptografa   = textoCriptografar.value;
-    console.log(textoParaCriptografa)
+    textoParaCriptografa   = textoCriptografar.value
+    const resultadoCriptografia = criptografar(textoParaCriptografa)
+    document.getElementById("texto-decriptografar").value = resultadoCriptografia;
 
 })
 
+const textoDescriptografar = document.getElementById("texto-decriptografar")
+const enviarDescriptografia = document.getElementById("enviar-desciptografia")
 
+enviarDescriptografia.addEventListener('click', function() {
 
-function criptografar() {
-    let texto = document.getElementById("texto-criptografar");
+    textoParaDescriptografar = textoDescriptografar.value
+    const resultadoDescriptografia = descriptografar(textoParaDescriptografar)
+    document.getElementById("texto-criptografar").value = resultadoDescriptografia
 
-    for ( let letra of texto ) {
-
-        if(letra === a) {
-            texto[letra] = "ai"
-            
-        }
-        if(letra === e) {
-            texto[letra] = "enter"
-            
-        }
-        if(letra === i) {
-            texto[letra] = "imes"
-            
-        }
-        if(letra === o) {
-            texto[letra] = "ober"
-            
-        }
-        if(letra === u) {
-            texto[letra] = "ufat"
-            
-        }
-        console.log(texto)
-        
-    }
-}
+})
